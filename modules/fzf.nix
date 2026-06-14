9c08bb3371d23aa8244149d9da04374ce0df3490
Jona Corynen <jonacorynen@gmail.com>
2026-06-14 19:01:39 +0200

    update to fzf config

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -1,5 +1,9 @@
 {
-  home.sessionVariables."FZF_COMPLETION_OPTS" = "--height=~12";
+  home.sessionVariables = {
+    "FZF_COMPLETION_OPTS" = "--height=~12";
+    "FZF_COMPLETION_DIR_OPTS" = "--walker dir,follow,hidden";
+    "FZF_COMPLETION_PATH_OPTS" = "--walker file,dir,follow,hidden";
+  };
 
   programs.fzf = {
     enable = true;
@@ -21,7 +25,9 @@
       "label" = "#c6c8d1";
       "query" = "#c6c8d1";
     };
+    changeDirWidgetCommand = "find -L . -mindepth 1 -path '*/.*' -prune -o -type d -printf '%P/\\n' 2>/dev/null";
     changeDirWidgetOptions = [ "--height=~12" ];
+    defaultCommand = "find -L . -mindepth 1 -path '*/.*' -prune -o -not -type d -printf '%P\\n' 2>/dev/null";
     defaultOptions = [
       "--bind=tab:toggle+down,shift-tab:toggle+up"
       "--border=none"
@@ -30,6 +36,7 @@
       "--preview-border=sharp"
       "--scroll-off=3"
     ];
+    fileWidgetCommand = "find -L . -mindepth 1 -path '*/.*' -prune -o -type d -printf '%P/\\n' -o -not -type d -printf '%P\\n' 2>/dev/null";
     fileWidgetOptions = [ "--height=~12" ];
     historyWidgetOptions = [ "--height=~12" ];
   };
